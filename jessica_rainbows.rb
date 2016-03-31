58dd298838ece077ae6bded89c68893e4e3aa8d8
Jessica <jessica.weeber@gmail.com>
2016-03-31 09:31:38 -0700

    made rainbows from rgb yo

--- jessica_rainbows.rb
+++ jessica_rainbows.rb
@@ -1,6 +1,5 @@
 require 'rainbow/ext/string'
 
-def find_color(red, green, blue)
   rainbow = [
     { red: { r: 255, g: 0, b: 0 } },
     { orange: { r: 255, g: 127, b: 0 } },
@@ -11,18 +10,8 @@ def find_color(red, green, blue)
     { violet: { r: 143, g: 0, b: 255 } }
   ]
 
-  rainbow.each do |color|
-    color.each do |key, value|
-      return key.to_s if red == value[:r] && green == value[:g] && blue == value[:b]
+  rainbow.each do |item|
+    item.each do |key, value|
+     puts Rainbow(key.to_s).color(value[:r], value[:g], value[:b])
     end
   end
-
-end
-
-puts find_color(255, 0, 0).color(:red)
-puts find_color(255, 127, 0).color(:orange)
-puts find_color(255, 255, 0).color(:yellow)
-puts find_color(0, 255, 0).color(:green)
-puts find_color(0, 0, 255).color(:blue)
-puts find_color(75, 0, 130).color(:indigo)
-puts find_color(143, 0, 255).color(:violet)
